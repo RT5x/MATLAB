dc0d4927fbec2bb381591f1336b31a9b989c0154
Adam Ghatta <67446944+RT5x@users.noreply.github.com>
2022-12-27 10:33:09 -0800

    Attempt 2 (Saved, Incorrect)

--- MATLAB Cody Problems/Problem 93 (Hard)/Solution 93.m
+++ MATLAB Cody Problems/Problem 93 (Hard)/Solution 93.m
@@ -1,1 +1,18 @@
+s1 = 'creation'
+s2 = 'creations'
 
+l1 = length(s1);
+l2 = length(s2);
+if l1 == l2  % Case 1: s1 and s2 have same length
+    
+    if s1 == s2  % Case 2: s1 and s2 are the same string
+        d = 0
+    else
+        array = s1~=s2;
+        d = nnz(array)
+    end
+   
+else    % Case 3: s1 and s2 have different lengths
+     0;   
+    
+end
