dd9808d57bbf8db20e04ea12214c81b5d3e820a0
Adam Ghatta <67446944+RT5x@users.noreply.github.com>
2023-01-10 12:01:30 -0800

    Update Solution 69.m

--- MATLAB Cody Problems/Problem 69 (Medium)/Solution 69.m
+++ MATLAB Cody Problems/Problem 69 (Medium)/Solution 69.m
@@ -1,16 +1,21 @@
 function pmax = peakOfPeaks(nmax)
 c = ones(nmax);
 c(1:nmax,1) = 1:nmax;
+i = 1;
+
 for j = 1:nmax
-    for i = 1:nmax
+    for i = 1:1e4
         if mod(c(j,i),2) == 0;
             c(j,i+1) = c(j,i) / 2;
             
         else
             c(j,i+1)=(3*c(j,i) + 1);
+  
             
         end
-        
+        %if c(j,i+1) == 4 || c(j,i+1) == 2 || c(j,i+1)==1;
+           % j = j+1;
+        %end
     end
 end
 
